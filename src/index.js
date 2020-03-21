7c217f17b0556695aed24fdee11f83f20c8e6c5d
Andrei <58465851+gorestfump1@users.noreply.github.com>
2020-03-21 10:28:34 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+const first = [' ','one','two','three','four','five','six','seven','eight','nine'];
+    const teen = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    const tens = [' ',' ','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+
 module.exports = function toReadable (number) {
-  
+    let result = []
+    if ( number === 0 ) { return 'zero'}
+    if ( number > 99 && number < 1000 ) { let num = (number / 100 ^ 0); result.push(first[num]); result.push('hundred'); number -= (num*100) }
+    if ( number > 19 && number < 100 ) { let num = (number / 10 ^ 0); result.push(tens[num]); number -= (num*10)}
+    if ( number > 9 && number < 20 ) { result.push(teen[(number-10)]) }
+    if ( number > 0 && number < 10 ) { result.push(first[number]) }
+    return result.join(' ')
 }
